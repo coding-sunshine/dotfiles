10466a9a702b74dfac456e3108e66678954f9328
Sunshine <sunshine.mahant@gmail.com>
2020-09-03 03:47:52 +1000

    removed old php

--- custom_commands.zsh
+++ custom_commands.zsh
@@ -1,6 +1,6 @@
 phpv() {
     valet stop
-    brew unlink php@7.0 php@7.1 php@7.2 php@7.3 php@7.4
+    brew unlink php@7.2 php@7.3 php@7.4
     brew link --force --overwrite $1
     brew services start $1
     composer global update
