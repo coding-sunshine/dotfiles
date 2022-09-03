b2b01dc9876fd2a623eb0a5603b487698566b3cf
Sunshine <sunshine.mahant@gmail.com>
2022-09-03 14:03:48 +0530

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -27,7 +27,7 @@ fi
 
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 echo "Installing xcode-stuff"
